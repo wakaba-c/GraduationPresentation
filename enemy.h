69ff2f4694726bb5f1b833f48c0fd250e2c534fa
masayasu wakita <koamchi.w315@gmail.com>
2020-10-15 10:10:42 +0900

    敵の調整をしました。

--- enemy.h
+++ enemy.h
@@ -11,12 +11,7 @@
 // インクルードファイル
 //=============================================================================
 #include "main.h"
-#include "scene.h"
-
-//=============================================================================
-// マクロ定義
-//=============================================================================
-#define MAX_KEY 30				// キーの最大値
+#include "character.h"
 
 //=============================================================================
 // 列挙体定義
@@ -33,18 +28,15 @@ typedef enum
 //=============================================================================
 // 前方宣言
 //=============================================================================
-class CMeshCapsule;
 class CColliderSphere;
 class CColliderBox;
-class CMeshCube;
 class CCollider;
 class CLife;
-class CModel;
 
 //=============================================================================
 // クラス定義
 //=============================================================================
-class CEnemy : public CScene
+class CEnemy : public CCharacter
 {
 public:
 	//モーションタイプ
@@ -58,28 +50,6 @@ public:
 		ANIMATIONTYPE_DIE,
 		ANIMATIONTYPE_MAX
 	} ANIMATIONTYPE;
-
-	//キー要素
-	typedef struct
-	{
-		D3DXVECTOR3 pos;			// 位置
-		D3DXVECTOR3 rot;			// 回転
-	} KEY;
-
-	//キー情報
-	typedef struct
-	{
-		KEY aKey[MAX_KEY];			// パーツのキー情報
-		int nFrame;					// フレーム数
-	} KEY_INFO;
-
-	// アニメーション情報
-	typedef struct
-	{
-		int	nLoop;					// ループするかどうか
-		int	nMaxKey;				// キー数
-		KEY_INFO *apKeyInfo;		// キーの構造体
-	} ANIMATION;
 
 	CEnemy(PRIORITY obj);			// コンストラクタ
 	~CEnemy();						// デストラクタ
@@ -97,6 +67,9 @@ public:
 	void OnCollisionEnter(CCollider *col) {};
 	void ShowInspector(void) {};
 
+	void BehaviorForMaxFrame(void);				// 最大フレーム数に到達したときの処理
+	void BehaviorForMaxKey(void);				// 最大キー数に到達したときの処理
+
 	static int GetEnemyKill(void) { return m_nKill; }					// 倒した数の取得
 	D3DXMATRIX GetMtxWorld(void) { return m_mtxWorld; }					// マトリックスの取得
 	CColliderSphere *GetSphere(void) { return m_pSphere; }				// スフィアコライダーの取得
@@ -106,33 +79,11 @@ private:
 #ifdef _DEBUG
 	void Debug(void);													// デバッグ処理
 #endif
-	void LoadScript(void);												// スクリプトデータのロード関数
 	void Move(D3DXVECTOR3 &pPos);										// 移動処理
-	void Animation(void);												// アニメーション関連関数
-	void AnimationSwitch(ANIMATIONTYPE Type);							// アニメーションの切り替え処理
 	void Collider(void);												// 当たり判定マネージャ
 
 	static LPDIRECT3DTEXTURE9		m_pTexture;							// テクスチャへのポインタ
 	D3DXMATRIX						m_mtxWorld;							// ワールドマトリックス
-
-	/*===============3Dレンダリング関連===============*/
-	LPDIRECT3DVERTEXBUFFER9			m_pVtxBuff;							// 頂点バッファへのポインタ
-	static LPD3DXMESH				m_pMesh;							// メッシュ情報へのポインタ
-	static DWORD					m_nNumMat;							// マテリアル情報の数
-	static LPD3DXBUFFER				m_pBuffMat;							// マテリアル情報へのポインタ
-	CModel							*m_pModel;							// パーツモデルのポインタ
-
-	/*===============アニメーション関連===============*/
-	int								m_nNumKey;							// キーの総数
-	int								m_nNumParts;						// パーツの総数
-	int								m_nCurrentKey;						// 現在のキーNo.
-	int								m_nCurrentFrame;					// 現在のフレームNo.
-	int								m_nCntMotion;						// カウンターモーション
-	ANIMATION						m_pAnimation[ANIMATIONTYPE_MAX];	// キー情報ポインタ
-	ANIMATIONTYPE					m_AnimationType;					// 現在のアニメーションタイプ
-	D3DXVECTOR3						*m_apCurrentPos;					// 1フレーム当たりの移動量
-	D3DXVECTOR3						*m_apCurrentRot;					// 1フレーム当たりの回転量
-	int								m_nInterval;						// 休憩時間
 
 	/*=============== 当たり判定 ===============*/
 	CColliderSphere					*m_pSphere;							// 当たり判定(体)のポインタ
@@ -146,6 +97,7 @@ private:
 	D3DXVECTOR3						m_dest;								// 移動先
 	D3DXVECTOR3						m_difference;						// 差
 	int								m_nLife;							// 体力
+	int								m_nInterval;						// 休憩時間
 	bool							m_bAnimSwitch;						// アニメーションの有無
 	bool							m_bHit;								// 当たり判定
 	bool							m_bJump;							// ジャンプ
