8f1d836c515093865b152e23e46ae489c73fc267
masayasu wakita <koamchi.w315@gmail.com>
2020-12-17 15:01:38 +0900

    ゴール用アセットとモデルデータを入れました。

--- game.cpp
+++ game.cpp
@@ -37,6 +37,7 @@
 #include "network.h"
 #include "startSignal.h"
 #include "shadow.h"
+#include "finishUi.h"
 
 //=============================================================================
 // Ã“Iƒƒ“ƒo•Ï”
@@ -157,6 +158,10 @@ void CGame::Update(void)
 	if (pKeyboard->GetTriggerKeyboard(DIK_0))
 	{
 		CStartSignal::Create();
+	}
+	if (pKeyboard->GetTriggerKeyboard(DIK_7))
+	{
+		CFinishUi::Create();
 	}
 #endif
 
